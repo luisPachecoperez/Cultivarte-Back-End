4a6b5b9362cc67aee1b7eaf663a621eeb7fb3b7a
Orlando Vasquez <orlando.vasquez@yahoo.com>
2025-07-28 16:26:13 -0500

    Ajuste para conexion ssl require  y conexion a base de datos nube

--- src/infrastructure/db/pg-pool.ts
+++ src/infrastructure/db/pg-pool.ts
@@ -1,9 +1,12 @@
 import { Pool } from 'pg';
 
 export const pgPool = new Pool({
-  host: 'localhost',
-  port: 5432,
-  user: 'postgres',
-  password: '',
-  database: 'eventos',
+  host: '209.151.149.65',
+  port: 17873,
+  user: 'avnadmin',
+  password: 'AVNS_I8LEBN1f5ENDdq_-a3P',
+  database: 'defaultdb',
+  ssl: {
+    rejectUnauthorized: false // ← necesario para conexiones con SSL (modo 'require')
+  }
 });
