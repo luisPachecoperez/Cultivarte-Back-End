b457704efbf792e6a9e147ed1a701fc9bdbddd84
brayancig-crea <brayancig@gmail.com>
2025-09-01 09:59:09 -0500

    Se sube la conexion a labase de datos (BCIG)

--- src/infrastructure/db/pg-pool.ts
+++ src/infrastructure/db/pg-pool.ts
@@ -1,12 +1,16 @@
 import { Pool } from 'pg';
-
+import fs from 'fs';
+import path from 'path';
 export const pgPool = new Pool({
-  host: '209.151.149.65',
+  
+  host: 'fbd-orlandojvasquez74-6861.g.aivencloud.com',
   port: 17873,
   user: 'avnadmin',
   password: 'AVNS_I8LEBN1f5ENDdq_-a3P',
   database: 'defaultdb',
   ssl: {
-    rejectUnauthorized: false // ← necesario para conexiones con SSL (modo 'require')
+    rejectUnauthorized: false,
+    ca: fs.readFileSync(path.join(__dirname, '../../../certs/ca.pem')).toString()
   }
+
 });
