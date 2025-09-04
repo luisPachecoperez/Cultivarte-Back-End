2e175f9ff12ed882bc061ad326ccfbd4cc6fde92
Orlando Vasquez <orlando.vasquez@yahoo.com>
2025-09-04 17:13:24 -0500

    Arreglo de personas programas

--- src/infrastructure/db/personas-programa-queries.ts
+++ src/infrastructure/db/personas-programa-queries.ts
@@ -1,7 +1,7 @@
 export const personasProgramaQueries = {
-    getAll: 'SELECT * FROM personas_programa',
-    getById: 'SELECT * FROM personas_programa WHERE id_persona_programa = $1',
-    create: 'INSERT INTO personas_programa (id_persona_programa, id_persona, id_programa) VALUES ($1, $2, $3)',
-    updateById: 'UPDATE personas_programa SET id_persona = $2, id_programa = $3 WHERE id_persona_programa = $1',
-    deleteById: 'DELETE FROM personas_programa WHERE id_persona_programa = $1'
+    getAll: 'SELECT * FROM personas_programas',
+    getById: 'SELECT * FROM personas_programas WHERE id_persona_programa = $1',
+    create: 'INSERT INTO personas_programas (id_persona_programa, id_persona, id_programa) VALUES ($1, $2, $3)',
+    updateById: 'UPDATE personas_programas SET id_persona = $2, id_programa = $3 WHERE id_persona_programa = $1',
+    deleteById: 'DELETE FROM personas_programas WHERE id_persona_programa = $1'
 };
